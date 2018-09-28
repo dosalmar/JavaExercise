d9ef393bbff812ca3264c94b53d4333785caf1f0
marta.dominguez <marta.dominguez@privalia.com>
2018-09-28 08:59:05 +0200

    end of the day

---
+++ src/main/java/com/privalia/exam/Calendar.java
@@ -0,0 +1,16 @@
+package com.privalia.exam;
+
+import java.util.GregorianCalendar;
+
+
+
+public class Calendar extends GregorianCalendar{
+
+	
+	@Override
+	public String toString() {
+		String s = "a"
+				
+		return a;
+	}
+}
--- src/main/java/com/privalia/exam/Main.java
+++ src/main/java/com/privalia/exam/Main.java
@@ -1,29 +1,46 @@
 package com.privalia.exam;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 
 		
-		Calendar cal = new GregorianCalendar(2018, 4, 0);
-		cal.setLenient(true);
-		
-		
-		cal.set(GregorianCalendar.DAY_OF_WEEK, Calendar.THURSDAY);
-		cal.set(GregorianCalendar.DAY_OF_WEEK_IN_MONTH, -1);
-		
-		
-		System.out.println("Last thursday: " + cal.getTime());
-		cal.add(Calendar.DAY_OF_MONTH, 1);
-		System.out.println("Next day after last Thursday: " + cal.getTime());
-		
-		
+		Calendar from = new GregorianCalendar(2017, 0, 0);
+		Calendar to = new GregorianCalendar(2017, 11, 0);
+
 		
 	}
 
 	
 	//Coger BigDecimals para no tener problema de float!!!
+	
+	
+	
+	
+	
+	
+	public ArrayList<Calendar> getThursdaysFromTo(Calendar from, Calendar to){
+		//ToDo: buscar todos los dias que hace falta hacer el calculo. 
+		
+		
+		ArrayList<Calendar> allThursdays = null;
+	
+		return allThursdays;
+	}
+	
+	
+	public Calendar getLastThursday(int year, int month, int day) {
+		Calendar cal = new GregorianCalendar(year, month, day);
+		
+		cal.set(GregorianCalendar.DAY_OF_WEEK, Calendar.THURSDAY);
+		cal.set(GregorianCalendar.DAY_OF_WEEK_IN_MONTH, -1);
+		
+		return cal;	
+	}
+	
 }
